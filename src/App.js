401b81a72b1bd6838f56366decd73e32df809145
SrishtiDharwani <srishti.dharwani.30@gmail.com>
2022-08-03 13:00:04 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ import "./App.css";
 
 const App = () => {
   return (
-    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+    <React.Fragment>
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
-      <Footer />
     </Box>
+      <Footer />
+    </React.Fragment>
+    
   );
 };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,14 @@ import Logo from "../assets/images/dumbbell.png";
 
 const Footer = () => {
   return (
-    <Box mt='80px' bgcolor="#fff3f4">
-      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
-      <img src={Logo} alt="logo" width='200px' height='40px' />
-        <Typography variant='h5' pb='40px' mt='20px'>
-          fg
+    <Box mt='80px' pb='20px' bgcolor="#fff3f4" alignItems='center' width="100vw">
+      <Stack gap='40px' alignItems='center' px='40px' justifyContent='center' pt='15px' direction="row">
+      <img src={Logo} alt="logo" width='60px' height='60px'/>
+        <Typography pb='40px' mt='20px' fontWeight={600} fontSize='20px' color='#ff2526' alignItems='center' letterSpacing={3} lineHeight='50px'>
+          Work-It-Out
         </Typography>
       </Stack>
+      <Typography variant='h6' align='center'>Made with ❤️. </Typography>
     </Box>
   )
 }
