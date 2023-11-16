30236ef66770a7235b1e6a95f770a9da8a2ce685
Srishti Dharwani <srishti.dharwani.30@gmail.com>
2023-11-17 03:28:13 +0530

    footer removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import {Box, Stack,Typography} from "@mui/material";
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 import Logo from "../assets/images/dumbbell.png";
 
 const Footer = () => {
   return (
-    <Box mt='80px' pb='20px' bgcolor="#fff3f4" alignItems='center' width="100vw">
-      <Stack gap='40px' alignItems='center' px='40px' justifyContent='center' pt='15px' direction="row">
-      <img src={Logo} alt="logo" width='40px' height='40px'/>
-        <Typography pb='40px' mt='20px' fontWeight={500} fontSize='30px' color='#ff2526' alignItems='center' letterSpacing={3} lineHeight='50px'>
+    <Box
+      mt="80px"
+      pb="20px"
+      bgcolor="#fff3f4"
+      alignItems="center"
+      width="100vw"
+    >
+      <Stack
+        gap="40px"
+        alignItems="center"
+        px="40px"
+        justifyContent="center"
+        pt="15px"
+        direction="row"
+      >
+        <img src={Logo} alt="logo" width="40px" height="40px" />
+        <Typography
+          pb="40px"
+          mt="20px"
+          fontWeight={500}
+          fontSize="30px"
+          color="#ff2526"
+          alignItems="center"
+          letterSpacing={3}
+          lineHeight="50px"
+        >
           Work-It-Out
         </Typography>
       </Stack>
-      <Typography variant='h6' align='center'>Made with ❤️ by Srishti. </Typography>
+      {/* <Typography variant='h6' align='center'>Made with ❤️ by Srishti. </Typography> */}
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
